f528af50ca5cfe21c0a333a79606e16bb4a3a51f
Snakeronie <34172148+Snakeronie@users.noreply.github.com>
2019-03-28 12:53:51 +0100

    Add files via upload
    first upload

---
+++ main.js
@@ -0,0 +1,151 @@
+var mygeoJSON = {
+"type": "FeatureCollection",
+"name": "alles",
+"features": [
+{ "type": "Feature", "properties": { "Name": "supermarkt_1", "description": "supermarkt" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2205959, 52.3745726 ], [ 5.2206536, 52.374435 ], [ 5.2211672, 52.3745136 ], [ 5.2211042, 52.3746594 ], [ 5.2205959, 52.3745726 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "supermarkt_2", "description": "supermarkt" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2204303, 52.3747511 ], [ 5.2201899, 52.3747148 ], [ 5.2202308, 52.3746358 ], [ 5.2205194, 52.3746798 ], [ 5.2204946, 52.3747269 ], [ 5.2204457, 52.3747191 ], [ 5.2204303, 52.3747511 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "supermarkt_3", "description": "supermarkt" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2430965, 52.376074 ], [ 5.2431635, 52.3757531 ], [ 5.2438877, 52.3758088 ], [ 5.2439092, 52.3761412 ], [ 5.2430965, 52.376074 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "supermarkt_4", "description": "supermarkt" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2159271, 52.3693314 ], [ 5.2159861, 52.3691726 ], [ 5.2164555, 52.3692414 ], [ 5.2163885, 52.3693969 ], [ 5.2159271, 52.3693314 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "supermarkt_5", "description": "supermarkt" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2180218, 52.3680465 ], [ 5.2181425, 52.3673095 ], [ 5.2186682, 52.3673816 ], [ 5.2189847, 52.3681726 ], [ 5.2180218, 52.3680465 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "supermarkt_6", "description": "supermarkt" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2213217, 52.3728839 ], [ 5.2213887, 52.3727447 ], [ 5.221673, 52.372784 ], [ 5.2215817, 52.3729803 ], [ 5.2214154, 52.3729541 ], [ 5.2213792, 52.3730418 ], [ 5.2212627, 52.3730231 ], [ 5.2213217, 52.3728839 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "supermarkt_7", "description": "supermarkt" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2163722, 52.3705834 ], [ 5.2164527, 52.3703836 ], [ 5.216576, 52.3704049 ], [ 5.2166672, 52.370174 ], [ 5.2170293, 52.3702346 ], [ 5.2168443, 52.3706423 ], [ 5.2163722, 52.3705834 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "supermarkt_8", "description": "supermarkt" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2198566, 52.3749059 ], [ 5.2199585, 52.3749231 ], [ 5.2199532, 52.3749353 ], [ 5.220031, 52.3749476 ], [ 5.2200028, 52.3750148 ], [ 5.2199223, 52.3750008 ], [ 5.2198486, 52.374919 ], [ 5.2198566, 52.3749059 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "kinderopvang_1", "description": "kinderopvang"}, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2308844, 52.3740636 ], [ 5.2309514, 52.3739097 ], [ 5.2313484, 52.3739678 ], [ 5.2312894, 52.3741218 ], [ 5.2308844, 52.3740636 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "kinderopvang_2", "description": "kinderopvang"}, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2303929, 52.3762196 ], [ 5.2303915, 52.3761557 ], [ 5.2304881, 52.3761524 ], [ 5.2304921, 52.3762204 ], [ 5.2303929, 52.3762196 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "kinderopvang_3", "description": "kinderopvang" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2342082, 52.373462 ], [ 5.2341559, 52.3736274 ], [ 5.2340822, 52.3736168 ], [ 5.2339762, 52.3735988 ], [ 5.2338757, 52.3735742 ], [ 5.233936, 52.373408 ], [ 5.23405, 52.3734366 ], [ 5.2341613, 52.3734546 ], [ 5.2342082, 52.373462 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "kinderopvang_4", "description": "kinderopvang" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2341731, 52.3713896 ], [ 5.2341409, 52.3713012 ], [ 5.2342697, 52.3712848 ], [ 5.2342911, 52.3713798 ], [ 5.2341731, 52.3713896 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "kinderopvang_5", "description": "kinderopvang" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2435454, 52.3740018 ], [ 5.2435628, 52.3738839 ], [ 5.2435923, 52.3738847 ], [ 5.2436084, 52.3738258 ], [ 5.2438082, 52.3738356 ], [ 5.243785, 52.3739172 ], [ 5.2438495, 52.3739201 ], [ 5.2438444, 52.3739502 ], [ 5.2437924, 52.3739494 ], [ 5.2437787, 52.374019 ], [ 5.2435454, 52.3740018 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "kinderopvang_6", "description": "kinderopvang" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2282319, 52.3792296 ], [ 5.228256, 52.3791674 ], [ 5.2283928, 52.3791936 ], [ 5.2283982, 52.3792296 ], [ 5.2283821, 52.3792706 ], [ 5.2283311, 52.3792886 ], [ 5.2282775, 52.379282 ], [ 5.2282399, 52.3792607 ], [ 5.2282319, 52.3792296 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "kinderopvang_7", "description": "kinderopvang" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.2160941, 52.3786353 ], [ 5.2161209, 52.3785747 ], [ 5.2159841, 52.3785518 ], [ 5.2160083, 52.3785108 ], [ 5.2160405, 52.3785157 ], [ 5.2160753, 52.3784617 ], [ 5.2161317, 52.3784372 ], [ 5.216188, 52.3784224 ], [ 5.2163355, 52.3784503 ], [ 5.2163516, 52.3784159 ], [ 5.2164133, 52.3784241 ], [ 5.2164294, 52.3784437 ], [ 5.2164508, 52.3784715 ], [ 5.2164589, 52.3784977 ], [ 5.2164401, 52.3785223 ], [ 5.2163999, 52.3785059 ], [ 5.2163355, 52.3786402 ], [ 5.2162819, 52.3786598 ], [ 5.2160941, 52.3786353 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "kinderopvang_8", "description": "kinderopvang" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.212062, 52.3788305 ], [ 5.2119467, 52.3786218 ], [ 5.2121331, 52.3785817 ], [ 5.2122484, 52.3787937 ], [ 5.212062, 52.3788305 ] ] ] } },
+{ "type": "Feature", "properties": { "Name": "kinderopvang_9", "description": "kinderopvang" }, "geometry": { "type": "Polygon", "coordinates": [ [ [ 5.226752, 52.3798844 ], [ 5.2267976, 52.3797731 ], [ 5.2269532, 52.379796 ], [ 5.2269022, 52.379909 ], [ 5.226752, 52.3798844 ] ] ] } }
+]
+}
+
+
+var mystyle = {
+    "version": 8,
+    "name": "Mijn eigen Stijl",
+    "sources": {
+		"cartiqo":{
+			"type": "vector",
+			"tiles":  [
+				"https://ta.webmapper.nl/wm/cartiqo/{z}/{x}/{y}",
+				"https://tb.webmapper.nl/wm/cartiqo/{z}/{x}/{y}",
+				"https://tc.webmapper.nl/wm/cartiqo/{z}/{x}/{y}"
+			]
+		}
+},
+	"layers":[ 
+        { 
+            "id":  "background",
+            "type": "background",
+            "paint": {
+                "background-color":"#FFFFFF"
+                }
+        },
+		{
+            "id": "water",
+            "source": "cartiqo",
+            "source-layer": "water",
+            "type": "fill",
+            "paint": {
+                "fill-color": "#a0c8f0"
+            }
+        },
+		{
+			"id": "buildings",
+			"type": "line",
+			"source": "cartiqo",
+			"source-layer":"builtup",
+			"paint": {
+			"line-color": "#2fbcd8",
+			}
+		},
+		        {
+            "id": "nature",
+            "source": "cartiqo",
+            "source-layer": "natural",
+            "type": "fill",
+            "minzoom": 0,
+            "maxzoom": 22,
+            "paint": {
+                "fill-color": [
+                    "match",
+                    [
+                        "get",
+                        "type"
+                    ],
+                    "high",
+                    "#adce9b",
+                    "low",
+                    "#d5e5cb",
+                    "bare",
+                    "#FAF7DD",
+                    "#ffffff"
+                ]
+            }
+        },
+		{
+			"id": "road",
+			"type": "line",
+			"source": "cartiqo",
+			"source-layer":"roads",
+			"paint": {
+			"line-color": "#eae393",
+			"line-width": 3
+			}
+		},
+				{
+			"id": "rail",
+			"type": "line",
+			"source": "cartiqo",
+			"source-layer":"railways",
+			"paint": {
+			"line-color": "#722c2c",
+			"line-width": 3
+			}
+		},
+		
+    ]
+}
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: mystyle,
+    hash: true,
+    zoom: 14,
+    pitch: 0,
+    bearing: 0,
+    center: [5.225715, 52.374197]
+});
+
+// On Load add GeoJSON SOURCE and LAYER
+map.on('load', function (e) {
+    // ADD GEOJSON SOURCE
+    map.addSource('alles', {
+        'type': 'geojson',
+        'data': mygeoJSON
+    });
+    // ADD an extra layer
+    map.addLayer({
+        'id': 'geojson-points',
+        'type': 'fill',
+        'source': 'alles',
+        'layout': {},
+        "paint": {
+                "fill-color": [
+                    "match",
+                    [
+                        "get",
+                        "description"
+                    ],
+                    "supermarkt",
+                    "#00ffff",
+                    "kinderopvang",
+                    "#0000ff",
+					"#ffff00"
+                ]
+            }		
+    });
+});
